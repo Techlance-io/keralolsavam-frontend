6d4718ee61132268f79b7e462f1fc339494026dc
jaison080 <jaisondennis080@gmail.com>
2022-12-05 20:10:19 +0530

    Login Completed

--- src/pages/events/index.js
+++ src/pages/events/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import styles from '../../styles/events/Home.module.css'
 
 function Events() {
   return (
--- src/pages/results/[id].js
+++ src/pages/results/[id].js
@@ -1,4 +1,5 @@
 import React from 'react'
+import styles from '../../styles/results/Results.module.css'
 
 function EventResults() {
   return (
