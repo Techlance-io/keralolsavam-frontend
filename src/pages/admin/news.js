91fc3d4972ee9cf248ecb3697a68affea75eb81a
jaison080 <jaisondennis080@gmail.com>
2022-12-06 20:21:52 +0530

    Updated

--- src/pages/admin/news.js
+++ src/pages/admin/news.js
@@ -1,39 +1,51 @@
-import axios from 'axios'
-import React from 'react'
-import {useState, useEffect} from 'react'
-import Card from "../../components/news/card"
-import styles from "../../styles/admin/Dashboard.module.css";
-import Navbar from '../../components/Navbar/Navbar';
+import axios from "axios";
+import React from "react";
+import { useState, useEffect } from "react";
+import styles from "../../styles/admin/News.module.css";
+import Navbar from "../../components/Navbar/Navbar";
+import { Button } from "@mui/material";
 
-function news() {
-    const [news, setNews] = useState([])
-    useEffect(()=>{
-     axios.get(`${process.env.NEXT_PUBLIC_API_URL}/news`).then((res)=>{
-         setNews(res.data.news)
-     })
-    },[])
-    
+function News() {
+  const [news, setNews] = useState();
+  async function getNews() {
+    await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/news`).then((res) => {
+      setNews(res.data.news);
+    });
+  }
+  useEffect(() => {
+    getNews();
+  }, []);
+
   return (
     <div>
       <Navbar />
       <div className={styles.container}>
-     <div className={styles.header}> <div className={styles.heading}>News</div>
-        <div
-          className={styles.register_btn}
-          onClick={() => {
-            signOutOfGoogle();
-          }}
-        >
-          Logout
+        <div className={styles.header}>
+          {" "}
+          <div className={styles.heading}>News</div>
+          <div
+            className={styles.register_btn}
+            onClick={() => {
+              signOutOfGoogle();
+            }}
+          >
+            Logout
+          </div>
         </div>
+        <div className={styles.news_box}>
+          {news?.map((data) => (
+            <div className={styles.news}>
+              <div className={styles.news_heading}>{data.title}</div>
+              <div className={styles.buttons}>
+                <Button variant="contained" color="primary">Edit</Button>
+                <Button variant="contained" color="error">Delete</Button>
+              </div>
+            </div>
+          ))}
         </div>
-        {
-      news.map((data)=>
-      <Card title={data.title} id = {data.id}/>)
-        }
-        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default news
+export default News;
