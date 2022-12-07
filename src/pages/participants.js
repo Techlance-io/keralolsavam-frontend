001bfc87a3d723607fc92f6534bd6eac7c6c648e
Jagannathes <jagannatheshahi@gmail.com>
2022-12-08 01:00:45 +0530

    added data grid

--- src/pages/participants.js
+++ src/pages/participants.js
@@ -4,35 +4,81 @@ import Navbar from "../components/Navbar/Navbar";
 import { useState, useEffect } from "react";
 import styles from "../styles/Participant.module.css";
 import { DataGrid } from "@mui/x-data-grid";
+import { Button, TextField } from "@mui/material";
+import Pagination from '@mui/material/Pagination';
+
+
+
 
 function participants() {
   const [participants, setParticipants] = useState([]);
+  const [page, setPage] = useState(1);
+    const [totalPages, setTotalPages] = useState(1);
+  const pagination = 10;
+  
+  const handlePageChange = (event, p) => {
+    setPage(p);
+  };
+
+  const Footer = () => {
+    return (
+        <div style={{flexGrow:1, display:"flex", justifyContent:"center", width:"100%", alignItem:"center"}}>
+        <Pagination count={totalPages} page={page} onChange={handlePageChange} />
+        </div>
+    )
+  }
   const data = {
     columns: [
-      { field: "id", headerName: "ID", width: 70 },
-      { field: "name", headerName: "Name", width: 130 },
-      { field: "email", headerName: "Email", width: 130 },
-      { field: "phone", headerName: "Phone", width: 130 },
-      { field: "college", headerName: "College", width: 130 },
+      { field: "sl", headerName: "ID", width: 50 },
+      { field: "name", headerName: "Name", width: 200 },
+      { field: "sex", headerName: "Gender", width: 150 },
+      { field: "phone", headerName: "Phone", width: 200 },
+      { field: "address", headerName: "Address", width: 200 },
+      { field: "date", headerName: "Date of Birth", width: 150, type: "date",
+      valueFormatter: (params) => {
+        return new Date(params.value).toLocaleDateString();
+      }},
+      { field: "artEvents", headerName: "Events", width: 300 },
+      {
+        field: "Edit",
+        headerName: "",
+        width: "100",
+        renderCell: (props) => <Button  >Edit</Button>,
+      },
     ],
-    rows: [{ id: 1, lastName: "Snow", firstName: "Jon", age: 35 }],
+    rows: participants,
+    components: {
+        Footer: Footer,
+    },
   };
   useEffect(() => {
     axios
-      .get(`${process.env.NEXT_PUBLIC_API_URL}/auth/getAllUsers`)
+      .get(`${process.env.NEXT_PUBLIC_API_URL}/auth/getAllUsers?page=${page}&pagination=${pagination}`)
       .then((res) => {
+        res.data.users.forEach((user, index) => {
+          user.id = user._id;
+          user.sl = index+1+((page-1)*pagination);
+          delete user._id;
+        });
         setParticipants(res.data.users);
+        setTotalPages(res.data.pages);
+        console.log(res.data.pages);                                                                                    
+        console.log(res.data.users);
       });
-  }, []);
+  }, [page]);
   return (
     <>
       <Navbar />
       <div className={styles.container}>
-        <div className={styles.heading}>Participants</div>
+        <div className={styles.heading} style={{marginBottom:"7px"}}>Participants</div>
         <div className={styles.content}>
-          <div style={{ display: "flex", height: "100%", flexGrow:1 }}>
+          <div style={{ display: "flex", height: "100%", flexGrow: 1 }}>
             <div style={{ flexGrow: 1 }}>
-              <DataGrid autoHeight {...data} />
+              <DataGrid 
+               autoHeight 
+               disableColumnFilter
+               disableColumnMenu 
+               {...data} />
             </div>
           </div>
         </div>
