9223e9a478b427e81e407fcc537159983ffab2dd
jaison080 <jaisondennis080@gmail.com>
2022-12-07 21:30:16 +0530

    Added Events Page

---
+++ src/components/EventCard/EventCard.js
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import React from "react";
+import styles from "./EventCard.module.css";
+
+function EventCard(props) {
+  return (
+    <>
+      <div className={styles.card}>
+        <div className={styles.img_div}><Image className={styles.img} src={props.image} alt=""/></div>
+        <div className={styles.title}>{props.title}</div>
+      </div>
+    </>
+  );
+}
+
+export default EventCard;
--- src/pages/events/index.js
+++ src/pages/events/index.js
@@ -1,12 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
+import EventCard from "../../components/EventCard/EventCard";
 import Navbar from "../../components/Navbar/Navbar";
+import sportsData from "../../data/sportsData";
+import artsData from "../../data/artsData";
 import styles from "../../styles/events/Home.module.css";
 
 function Events() {
+  const [sports, setSports] = useState(true);
+  const handleChangeArts = () => {
+    setSports(false);
+  };
+  const handleChangeSports = () => {
+    setSports(true);
+  };
   return (
     <>
       <Navbar />
-      <div>Events</div>
+      <div className={styles.container}>
+        <div className={styles.heading}>Events</div>
+        <div className={styles.tabs}>
+          <div
+            className={sports ? styles.sub_heading_1 : styles.sub_heading}
+            onClick={() => {
+              handleChangeSports();
+            }}
+          >
+            Sports
+          </div>
+          <div
+            className={sports ? styles.sub_heading : styles.sub_heading_1}
+            onClick={() => {
+              handleChangeArts();
+            }}
+          >
+            Arts
+          </div>
+        </div>
+        <div className={styles.cards}>
+          {sports
+            ? sportsData.map((item, index) => {
+                return (
+                  <div key={index}>
+                    <EventCard image={item.image} title={item.name} />
+                  </div>
+                );
+              })
+            : artsData.map((item, index) => {
+                return (
+                  <div key={index}>
+                    <EventCard image={item.image} title={item.name} />
+                  </div>
+                );
+              })}
+          )
+        </div>
+      </div>
     </>
   );
 }
