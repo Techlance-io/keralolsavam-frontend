1395cfb0298f788b46071fcd28dae56db21c9d00
jaison080 <jaisondennis080@gmail.com>
2022-12-09 14:40:37 +0530

    Styled Official Dashboard

--- src/pages/official/dashboard/index.js
+++ src/pages/official/dashboard/index.js
@@ -1,19 +1,15 @@
 import { getAuth, signOut } from "firebase/auth";
 import { useRouter } from "next/router";
 import axios from "axios";
-import { AuthContext } from "../../../context/AuthContext";  
+import { AuthContext } from "../../../context/AuthContext";
 import React, { useContext, useEffect } from "react";
 import app from "../../../utils/firebase";
 import styles from "../../../styles/official/Dashboard.module.css";
-import styless from "../../../styles/official/Home.module.css";
 import footer from "../../../assets/png/footer.png";
 import Image from "next/image";
-import left from "../../../assets/png/left.png";
-import right from "../../../assets/png/right.png";
-import top from "../../../assets/png/top.png";
 import Navbar from "../../../components/Navbar/Navbar";
 import EventCard from "../../../components/EventCard/EventCard";
-
+import eventsData from "../../../data/eventsData";
 
 function OfficialDashboard() {
   const router = useRouter();
@@ -21,6 +17,12 @@ function OfficialDashboard() {
   const [events, setEvents] = React.useState([]);
   const [user, setUser] = React.useState(null);
   const { authToken } = useContext(AuthContext);
+  const getImage = (name) => {
+    // find name proeprty in events data array:
+    const event = eventsData.find((event) => event.name === name);
+    // return image property of found event:
+    return event?.image;
+  };
   async function signOutOfGoogle() {
     signOut(auth)
       .then(() => {
@@ -30,14 +32,14 @@ function OfficialDashboard() {
   }
 
   const getEvent = async () => {
-    if(!authToken) return;
+    if (!authToken) return;
     const response = await axios.get(
       `${process.env.NEXT_PUBLIC_API_URL}/officer`,
       {
-          headers: {
-            "x-auth-token": authToken,
-          }
-        }
+        headers: {
+          "x-auth-token": authToken,
+        },
+      }
     );
     console.log(response.data);
     setUser(response.data);
@@ -47,39 +49,44 @@ function OfficialDashboard() {
   useEffect(() => {
     getEvent();
   }, [authToken]);
+  console.log(events);
   return (
     <>
       <Navbar />
+
       <div className={styles.container}>
-        <div className={styles.heading}>Official Dashboard</div>
-        <Image src={right} alt="" className={styles.image_top} />
-        <div
-          className={styles.register_btn}
-          onClick={() => {
-            signOutOfGoogle();
-          }}
-        >
-          Logout
+        {/* <Image src={right} alt="" className={styles.image_top} /> */}
+        <div className={styles.header}>
+          <div className={styles.heading}>Official Dashboard</div>
+          <div
+            className={styles.register_btn}
+            onClick={() => {
+              signOutOfGoogle();
+            }}
+          >
+            Logout
+          </div>
         </div>
-        <Image src={left} alt="" className={styles.image_left} />
-        <Image src={top} alt="" className={styles.image_right} />
+        {/* <Image src={left} alt="" className={styles.image_left} />
+        <Image src={top} alt="" className={styles.image_right} /> */}
+
+        <div className={styles.cards}>
+          {events.map((event) => {
+            return (
+              <div
+                onClick={() => router.push(`/official/dashboard/${event._id}`)}
+              >
+                <EventCard title={event.name} image={getImage(event.name)} />
+              </div>
+            );
+          })}
+        </div>
       </div>
       <Image
         src={footer}
         alt=""
         style={{ width: "100vw", position: "relative", bottom: "0" }}
       />
-      <div className={styless.cards}>
-      {
-        events.map((event) => {
-          return (
-            <div onClick={() => router.push(`/official/dashboard/${event._id}`)}>
-              <EventCard title = {event.name}  />
-            </div>
-          );
-        })
-      }
-      </div>
     </>
   );
 }
