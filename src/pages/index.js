c03f632208e642bad84d5eb9a2652bac0356bf95
jaison080 <jaisondennis080@gmail.com>
2022-12-20 18:21:34 +0530

    Verification

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { About, Carousel, Hero, Navbar } from "../components";
 import Footer from "../components/Footer/Footer";
 import styles from "../styles/Home.module.css";
@@ -6,13 +7,19 @@ import CustomTitle from "../utils/customTitle";
 export default function Home() {
   return (
     <>
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="L38wP45ufxCQGxUwTBluZJ188DpJ3g_P6Pnk1rRm3-o"
+        />
+      </Head>
       <CustomTitle title="Home" />
       <Navbar />
       <Hero />
 
       <About />
       <Carousel />
-      <Footer/>
+      <Footer />
     </>
   );
 }
